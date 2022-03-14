c5dcc0e3dde656543b429a02081142c8c9a58d9d
Kutsa1 <kutsalgurlek99@gmail.com>
2022-03-14 19:24:40 +0300

    homework bussines-concretes package is added

--- solutionchallenge/src/main/java/com/example/solutionchallenge/business/tools/Messages.java
+++ solutionchallenge/src/main/java/com/example/solutionchallenge/business/tools/Messages.java
@@ -2,16 +2,21 @@ package com.example.solutionchallenge.business.tools;
 
 public final class Messages {
 
-    public static String commentNotFound = "Comment is can not found";
-    public static String commentListed = "Comments are listed";
+    public static String commentNotFound = "Comment is can not found.";
+    public static String commentListed = "Comments are listed.";
 
-    public static String userNotFound = "User is can not found";
+    public static String userNotFound = "User is can not found.";
 
 
-    public static String homeworkNotFound = "Homework is can not found";
+    public static String homeworkNotFound = "Homework is can not found.";
 
-    public static String commentSave = "Comment is saved successfully";
-    public static String commentUpdate = "Comment is updated by succesfully";
-    public static String commentDelete = "Comment is deleted by succesfully";
-    public static String commentAddedToUser = "Comment is added";
+    public static String commentSave = "Comment is saved successfully.";
+    public static String commentUpdate = "Comment is updated by succesfully.";
+    public static String commentDelete = "Comment is deleted by succesfully.";
+    public static String commentAddedToUser = "Comment is added.";
+    public static String homeworkListed = "Homeworks are listed.";
+    public static String homeworkSave = "Homework is saved.";
+    public static String homeworkUpdate = "Homework is updated.";
+    public static String homeworkDelete = "Homework is deleted";
+    public static String homeworkAddedToUser ="Homework is added to user";
 }
