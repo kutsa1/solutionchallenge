55d19443f0c9a5a989c19154e5711d5140929966
Kutsa1 <kutsalgurlek99@gmail.com>
2022-03-15 20:14:08 +0300

    Homework controller is added

--- solutionchallenge/src/main/java/com/example/solutionchallenge/api/controller/HomeworkController.java
+++ solutionchallenge/src/main/java/com/example/solutionchallenge/api/controller/HomeworkController.java
@@ -1,4 +1,20 @@
 package com.example.solutionchallenge.api.controller;
 
-public class HomeworkController {
+import com.example.solutionchallenge.business.abstracts.IHomeworkService;
+import com.example.solutionchallenge.core.api.ControllerBase;
+import com.example.solutionchallenge.entities.concretes.Homework;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/homeworks")
+@RestController
+public class HomeworkController extends ControllerBase<Homework, IHomeworkService> {
+
+    IHomeworkService iHomeworkService;
+
+    public HomeworkController(IHomeworkService iHomeworkService){
+        super(iHomeworkService);
+        this.iHomeworkService=iHomeworkService;
+    }
 }
