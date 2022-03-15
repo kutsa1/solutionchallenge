25b7eb5da0689ea283e4794ef1bed4186e81c43a
Kutsa1 <kutsalgurlek99@gmail.com>
2022-03-15 20:06:53 +0300

    Comment controller is added

--- solutionchallenge/src/main/java/com/example/solutionchallenge/api/controller/CommentController.java
+++ solutionchallenge/src/main/java/com/example/solutionchallenge/api/controller/CommentController.java
@@ -1,4 +1,33 @@
 package com.example.solutionchallenge.api.controller;
 
-public class CommentController {
+import com.example.solutionchallenge.business.abstracts.ICommentService;
+import com.example.solutionchallenge.core.api.ControllerBase;
+import com.example.solutionchallenge.core.utilities.results.ErrorResult;
+import com.example.solutionchallenge.entities.concretes.Comment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/comments")
+public class CommentController extends ControllerBase<Comment, ICommentService> {
+
+    ICommentService iCommentService;
+
+    public CommentController(ICommentService iCommentService){
+        super(iCommentService);
+        this.iCommentService = iCommentService;
+    }
+
+     @PostMapping("/addcommenttohomework")
+    ResponseEntity<?> addCommentToHomework(@RequestParam int commentId, int homeworkId){
+        var result = iCommentService.addCommentToHomework(commentId,homeworkId);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new ErrorResult(result.getMessage()),HttpStatus.BAD_REQUEST);
+     }
 }
--- solutionchallenge/src/main/java/com/example/solutionchallenge/api/controller/StudentController.java
+++ solutionchallenge/src/main/java/com/example/solutionchallenge/api/controller/StudentController.java
@@ -1,4 +1,20 @@
 package com.example.solutionchallenge.api.controller;
 
-public class StudentController {
+
+import com.example.solutionchallenge.business.abstracts.IStudentService;
+import com.example.solutionchallenge.core.api.ControllerBase;
+import com.example.solutionchallenge.entities.concretes.Student;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/students")
+public class StudentController extends ControllerBase<Student, IStudentService> {
+
+    IStudentService iStudentService;
+
+    public StudentController(IStudentService iStudentService){
+        super(iStudentService);
+        this.iStudentService = iStudentService;
+    }
 }
