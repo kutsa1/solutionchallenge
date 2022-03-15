4dbf5b4baa0ea6742e3ec3eb70313d249ea2e4e7
Kutsa1 <kutsalgurlek99@gmail.com>
2022-03-15 21:54:53 +0300

    addHomeworkToUser

--- solutionchallenge/src/main/java/com/example/solutionchallenge/api/controller/HomeworkController.java
+++ solutionchallenge/src/main/java/com/example/solutionchallenge/api/controller/HomeworkController.java
@@ -2,7 +2,11 @@ package com.example.solutionchallenge.api.controller;
 
 import com.example.solutionchallenge.business.abstracts.IHomeworkService;
 import com.example.solutionchallenge.core.api.ControllerBase;
+import com.example.solutionchallenge.core.utilities.results.ErrorResult;
 import com.example.solutionchallenge.entities.concretes.Homework;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +20,14 @@ public class HomeworkController extends ControllerBase<Homework, IHomeworkServic
     public HomeworkController(IHomeworkService iHomeworkService){
         super(iHomeworkService);
         this.iHomeworkService=iHomeworkService;
+    }
+
+    @PostMapping("/addhomeworktouser")
+    ResponseEntity<?> addHomeworkToUser(@RequestParam int homeworkId, int userId){
+        var result = iHomeworkService.addHomeworkToUser(homeworkId, userId);
+        if (result.isSuccess()){
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new ErrorResult(result.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
