716691c96856af52e7eb30f155985ff651841fce
ismailkrc57 <ismailkrc57@gmail.com>
2022-03-20 00:37:30 +0300

    role user ve student için feild duzenlendi

--- solutionchallenge/src/main/java/com/example/solutionchallenge/core/entities/Role.java
+++ solutionchallenge/src/main/java/com/example/solutionchallenge/core/entities/Role.java
@@ -1,6 +1,5 @@
 package com.example.solutionchallenge.core.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,7 +8,6 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.util.List;
 
 @Entity
 @Table(name = "roles")
@@ -19,19 +17,14 @@ import java.util.List;
 @NoArgsConstructor
 public class Role implements IEntity {
 
-    @ManyToMany
-    @JsonIgnore
-    List<User> users;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @NotNull
     @NotBlank
     @Column(unique = true)
     private String name;
 
-    public Role(int id, String name) {
-        this.name = name;
-        this.id = id;
-    }
 }
